9d7d5691204b6abe362365afa89e212440e9ff8c
Martin Lencina <martinandres987@gmail.com>
2024-02-20 15:54:59 -0300

    Initial commit

---
+++ src/test/java/com/mindata/challenge/w2m/superheroes/SuperheroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.challenge.w2m.superheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
