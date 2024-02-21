4b00b5196acb668af2d6cc4496dd7304b7ad026b
Martin Lencina <martinandres987@gmail.com>
2024-02-21 15:53:46 -0300

    Se agrego controller

---
+++ src/main/java/com/mindata/challenge/w2m/superheroes/controller/SuperheroController.java
@@ -0,0 +1,47 @@
+package com.mindata.challenge.w2m.superheroes.controller;
+
+import com.mindata.challenge.w2m.superheroes.model.Superhero;
+import com.mindata.challenge.w2m.superheroes.service.SuperheroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/superheroes")
+public class SuperheroController {
+
+    @Autowired
+    private SuperheroService superheroService;
+
+    @GetMapping
+    public List<Superhero> getAllSuperheroes() {
+        return superheroService.getAllSuperheroes();
+    }
+
+    @GetMapping("/{id}")
+    public Superhero getSuperheroById(@PathVariable Long id) {
+        return superheroService.getSuperheroById(id)
+                .orElseThrow(() -> new RuntimeException("Superhero not found with id: " + id));
+    }
+
+    @GetMapping("/search")
+    public List<Superhero> getSuperheroesByName(@RequestParam String name) {
+        return superheroService.getSuperheroesByName(name);
+    }
+
+    @PostMapping
+    public Superhero createSuperhero(@RequestBody Superhero superhero) {
+        return superheroService.createSuperhero(superhero);
+    }
+
+    @PutMapping("/{id}")
+    public Superhero updateSuperhero(@PathVariable Long id, @RequestBody Superhero superhero) {
+        return superheroService.updateSuperhero(id, superhero);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSuperhero(@PathVariable Long id) {
+        superheroService.deleteSuperhero(id);
+    }
+}
